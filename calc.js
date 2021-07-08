0ee02003e0369df52e612876e5875dc6e2ca00ac
mosky <smartmosky@gmail.com>
2021-07-08 19:12:25 -0400

    All single operator equations work, added functionality to Clear button. Need to set character limit of answers

--- calc.js
+++ calc.js
@@ -8,10 +8,24 @@ function multiply(num1, num2) {
     return num1 * num2
 };
 function divide(num1, num2) {
-    return num1 / num2
+    if (num2 === 0) {
+        return 'und'
+    } else {
+    return num1 / num2}
 };
 function calc(num1, operator, num2) {
-return operator(num1, num2)
+    let a =parseInt(num1);
+    let b =parseInt(num2);
+    if (operator === 'add') {
+    return add(a, b)} else 
+    if (operator === 'subtract') {
+            return subtract(a, b)} else 
+            if (operator === 'multiply') {
+                return multiply(a, b)
+            } else {
+                return divide(a, b)
+            };
+    
 }
    const screenContent = document.querySelector('#screenContent')
    screenArray = [0];
@@ -45,9 +59,17 @@ document.querySelectorAll('.operators').forEach(item => {
       let equal = document.querySelector('#equal');
       equal.addEventListener('click', () => {calcArray.push(screenArray);
     screenArray = calc(calcArray[0], calcArray[1], calcArray[2]);
-    setscreen()
+    setscreen();
+    screenArray = [0];
+    calcArray = [];
     })
 
+    let clear = document.querySelector('#clear');
+clear.addEventListener('click', () => 
+    {screenArray = [0];
+    calcArray = [];
+    setscreen();
+    })
       
 
 
