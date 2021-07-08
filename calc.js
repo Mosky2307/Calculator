c0b95639b50055c87918e3462040da107eed8c1e
mosky <smartmosky@gmail.com>
2021-07-08 14:57:51 -0400

    added some Ui

--- calc.js
+++ calc.js
@@ -1,12 +1,14 @@
 function add(num1, num2) {
     return num1 + num2
-}
+};
 function subtract(num1, num2) {
     return num1 - num2
-}
+};
 function multiply(num1, num2) {
     return num1 * num2
-}
+};
 function divide(num1, num2) {
     return num1 / num2
-}
+};
+function calc(num1, operator, num2) {
+   return operator(num1, num2)}
