ae6d67fa6242362a2dd8b64616248075cd9564e3
mosky <smartmosky@gmail.com>
2021-07-08 16:18:07 -0400

    Reordered the buttons because I forgot the divide button lolololololol. Whole bunch of other stuff behind the scenes too

--- calc.js
+++ calc.js
@@ -12,18 +12,36 @@ function divide(num1, num2) {
 };
 function calc(num1, operator, num2) {
    return operator(num1, num2)};
-   const one = document.querySelector('#one');
    const screenContent = document.querySelector('#screenContent')
-   screenArray = [0]
+   screenArray = [0];
    function setscreen() {
        screenContent.textContent = screenArray;};
        document.querySelectorAll('.num').forEach(item => {
         item.addEventListener('click', () => {
+            if (screenArray[0] === 0) {
+                screenArray = []
+            };
             if (screenArray.length < 7) {
           screenArray += item.textContent;
           setscreen();}
         })
-      })
+      });
+calcArray = [];
+document.querySelectorAll('.operators').forEach(item => {
+    item.addEventListener('click', () => { calcArray.push(screenArray);
+    calcArray.push(item.id)
+    })
+})
+
+
+
+
+
+
+
+      let equal = document.querySelector('#equal');
+      equal.addEventListener('click', () => {calcArray.push(screenArray)})
+
       
 
 
