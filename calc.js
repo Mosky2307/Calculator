0b1623d592f5be3b290e8e0296c8d5886fcc867d
mosky <smartmosky@gmail.com>
2021-07-08 15:39:22 -0400

    Made number buttons add those numbers to the number on screen and set a number limit of 7

--- calc.js
+++ calc.js
@@ -11,4 +11,22 @@ function divide(num1, num2) {
     return num1 / num2
 };
 function calc(num1, operator, num2) {
-   return operator(num1, num2)}
+   return operator(num1, num2)};
+   const one = document.querySelector('#one');
+   const screenContent = document.querySelector('#screenContent')
+   screenArray = [0]
+   function setscreen() {
+       screenContent.textContent = screenArray;};
+       document.querySelectorAll('.num').forEach(item => {
+        item.addEventListener('click', () => {
+            if (screenArray.length < 7) {
+          screenArray += item.textContent;
+          setscreen();}
+        })
+      })
+      
+
+
+
+
+setscreen();
